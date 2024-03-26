f5dc37ee5758dd7e601d7ce1d493e7a9b8d24410
Kristian <kristiangcorrea@live.com>
2024-03-26 15:08:08 -0400

    Converted logo pngs into SVG's. Created SVG Components. Now SVG's change colors depending if browser is in light or dark mode.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        foreground: 'rgb(var(--foreground-rgb))',
+        backgroundColor: 'rgb(var(--background-start-rgb))'
+
+      }
     },
   },
   plugins: [],
