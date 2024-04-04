b8c9e7dae8056920d354451f8ec842f12186aab2
Kristian <kristiangcorrea@live.com>
2024-04-03 23:50:28 -0400

    Created project are with projectitem sub component.

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import Main from "@/components/Main";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
     <main className="bg-backgroundColor flex min-h-screen flex-col items-center justify-between p-24 pt-0">
       <Navbar/>
+      <Main />
+      <Projects />
     </main>
   );
 }
